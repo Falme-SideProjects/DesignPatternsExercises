28432962ffae1b984491b995669ce2c386cf05bb
Falme Streamless <falme_streamless@hotmail.com>
2020-07-22 05:46:39 -0300

    Created Factory Method

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryMethod factoryMethod = new FactoryMethod();
+
+            Console.WriteLine("Liu Kang | SubZero | Scorpion");
+            Console.WriteLine();
+
+            Console.WriteLine("Selecione seu personagem");
+            string escolha = Console.ReadLine();
+
+            IPersonagem personagem = factoryMethod.Escolher_Personagem(escolha);
+
+            Console.WriteLine();
+            Console.WriteLine("Voce Vai jogar com : ");
+            personagem.Escolhido();
+
+            Console.ReadKey();
+
+        }
+    }
+}
